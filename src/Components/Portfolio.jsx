0c05d6d277070b843818c9e44e73a45d88dacf74
dominionolonilebi <dominionolufunke27@gmail.com>
2024-12-15 16:41:37 +0100

    updated

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -26,7 +26,7 @@ const Portfolio = () => {
               WEB DESIGN
             </Nav.Link>
           </Nav.Item>
-          <Nav.Item className="portfolio">
+          <Nav.Item className="portfolio">git
             <Nav.Link href="/" className="text-dark">
               PHOTOGRAPHY
             </Nav.Link>
@@ -36,14 +36,17 @@ const Portfolio = () => {
           <Col md={4}>
             <h1>95%</h1>
             <h6>Branding</h6>
+            <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat totam nesciunt, at assumenda porro ipsum odit vitae pariatur mollitia asperiores, molestias, autem maiores architecto molestiae delectus modi amet libero eius.</p>
           </Col>
           <Col md={4}>
             <h1>98%</h1>
             <h6>Web Design</h6>
+            <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat totam nesciunt, at assumenda porro ipsum odit vitae pariatur mollitia asperiores, molestias, autem maiores architecto molestiae delectus modi amet libero eius.</p>
           </Col>
           <Col md={4}>
             <h1>90%</h1>
             <h6>Photography</h6>
+            <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat totam nesciunt, at assumenda porro ipsum odit vitae pariatur mollitia asperiores, molestias, autem maiores architecto molestiae delectus modi amet libero eius.</p>
           </Col>
         </Row>
       </Container>
